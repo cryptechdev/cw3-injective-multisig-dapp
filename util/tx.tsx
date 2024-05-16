125726a9fb98d001b0bde7c7f8662672728de9b9
Eric Woolsey <ewoolsey@ualberta.ca>
2024-05-16 11:14:58 -0700

    for logan

--- util/tx.tsx
+++ util/tx.tsx
@@ -1,49 +1,30 @@
-import { MsgInstantiateContractEncodeObject } from "@cosmjs/cosmwasm-stargate";
-import { MsgInstantiateContract } from "@injectivelabs/sdk-ts";
-import { MsgExecuteContractEncodeObject } from "cosmwasm";
-import { InstantiateQueryClient } from "ts/Instantiate.client";
-import { InstantiateMsgComposer } from "ts/Instantiate.message-composer";
-import { InstantiateMsg } from "types/injective-cw3";
+import { MsgInstantiateContractEncodeObject } from '@cosmjs/cosmwasm-stargate'
+import { toUtf8 } from 'cosmwasm'
+import { InstantiateMsg } from 'types/injective-cw3'
+import { MsgInstantiateContract } from 'cosmjs-types/cosmwasm/wasm/v1/tx'
 
 export const instantiateMultisigTx = async (
   walletAddress: string,
   message: InstantiateMsg,
-  executeTx: (msgs: InstantiateMsgComposer[]) => Promise<
+  executeTx: (msgs: MsgInstantiateContractEncodeObject[]) => Promise<
     | {
-        transactionHash: string;
-      }
+      transactionHash: string
+    }
     | undefined
   >
 ): Promise<{ transactionHash: string } | undefined> => {
-  let msg: InstantiateMsgComposer;
-  if (message) {
-    const params: InstantiateMsg = {
-      sender: message.sender,
-      admin: message.admin,
-      codeId: message.codeId,
-      label: message.label,
-      msg: message.msg,
-      max_voting_period: {
-        time: 0
-      },
-      threshold: {
-        absolute_count: {
-          weight: 0
-        }
-      },
-      voters: []
-    };
-    const msgComposer = new MsgInstantiateContract(
-      params
-    );
-    msg = msgComposer.execute(
-      {
-        proposalId: message.proposalId,
-      },
-      message.funds
-    );
+  let msg: MsgInstantiateContractEncodeObject
+  msg = {
+    typeUrl: '/cosmwasm.wasm.v1.MsgInstantiateContract',
+    value: MsgInstantiateContract.fromPartial({
+      sender: this.sender,
+      admin: this.admin,
+      label: this.label,
+      code_id: this.code_id,
+      msg: toUtf8(JSON.stringify(message)),
+      funds: [],
+    }),
   }
 
-  return await executeTx([msg]);
-
-};
+  return await executeTx([msg])
+}
